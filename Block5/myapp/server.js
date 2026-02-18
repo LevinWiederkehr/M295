ba1a7b037108afb904e6d3ce1b72334fa7467636
Levin Wiederkehr <levin.wiederkehr@edu.tbz.ch>
2026-02-18 10:38:41 +0100

    Block 5 Update -> Block6 Swagger | Blcok 6 Aufgabe 6.2 swagger-ui

--- Block5/myapp/server.js
+++ Block5/myapp/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const bookRoutes = require('./routes/books');
 const lendRoutes = require('./routes/lends');
 
@@ -6,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use('/books', bookRoutes);
